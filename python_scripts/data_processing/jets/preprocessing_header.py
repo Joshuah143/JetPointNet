4916b150eb51202bc3adb9fdeee66e910b2d45f0
clissa <clissa.professional@gmail.com>
2024-05-17 20:09:43 +0200

    Add config for ttbar dataset

--- python_scripts/data_processing/jets/preprocessing_header.py
+++ python_scripts/data_processing/jets/preprocessing_header.py
@@ -23,7 +23,8 @@ VAL_SPLIT_RATIO = 0.3
 # TEST_SPLIT_RATIO is implied to be the remaining percentage
 NUM_THREAD_PER_CHUNK = 25  # root to awk
 NUM_CHUNK_THREADS = 16  # awk to npz
-OUTPUT_DIRECTORY_NAME = "rho_full/"
+# OUTPUT_DIRECTORY_NAME = "rho_full/"
+OUTPUT_DIRECTORY_NAME = "ttbar/"
 # ============================
 
 
@@ -34,13 +35,14 @@ MAX_DISTANCE = 0.2
 # Path to the ROOT file containing jet events
 
 # RHO DATA
-FILE_LOC = "/eos/home-m/mswiatlo/images/truthPerCell/rho_full.root"
-GEO_FILE_LOC = "/eos/home-m/mswiatlo/images/truthPerCell/cell_geo.root"
+# FILE_LOC = "/eos/home-m/mswiatlo/images/truthPerCell/rho_full.root"
+# GEO_FILE_LOC = "/eos/home-m/mswiatlo/images/truthPerCell/cell_geo.root"
 
 # JETS DATA
 # FILE_LOC = "/data/atlas/mltree_2000_fixedHits.root"
 # GEO_FILE_LOC = "/data/atlas/data/rho_delta/rho_small.root"
-
+FILE_LOC = "/eos/home-m/mswiatlo/forLuca/mltree_large.root"
+GEO_FILE_LOC = "/eos/home-m/mswiatlo/images/truthPerCell/cell_geo.root"
 
 AWK_SAVE_LOC = (
     REPO_PATH
