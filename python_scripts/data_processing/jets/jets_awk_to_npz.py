899f52120e8be4cd75f29d59cfbd2f4fcdf5555b
clissa <clissa.professional@gmail.com>
2024-05-20 11:04:15 +0200

    Implement multiprocessing for awk to npz conversion

--- python_scripts/data_processing/jets/jets_awk_to_npz.py
+++ python_scripts/data_processing/jets/jets_awk_to_npz.py
@@ -13,7 +13,11 @@ from data_processing.jets.util_functs import (
     build_labels_array,
     build_input_array,
 )
-from data_processing.jets.preprocessing_header import AWK_SAVE_LOC, NPZ_SAVE_LOC
+from data_processing.jets.preprocessing_header import (
+    AWK_SAVE_LOC,
+    NPZ_SAVE_LOC,
+    NUM_CHUNK_THREADS,
+)
 import awkward as ak
 import pyarrow.parquet as pq
 import numpy as np
@@ -21,22 +25,16 @@ import pandas as pd
 import os
 import time
 from tqdm.auto import tqdm
+from multiprocessing import Pool
 
-DATA_FOLDERS = ["train", "test", "val"]
+
+DATA_FOLDERS = ["train", "val", "test"]
 
 
 def read_parquet(filename):
     table = pq.read_table(filename)
     ak_array = ak.from_arrow(table)
     return ak_array
-
-
-# Make sure this happens after SAVE_LOC is defined and created if necessary
-for folder in DATA_FOLDERS:
-    folder_path = os.path.join(AWK_SAVE_LOC, folder)
-    os.makedirs(
-        folder_path, exist_ok=True
-    )  # This line ensures the AWK_SAVE_LOC directories exist
 
 
 def find_global_max_sample_length():
@@ -73,39 +71,72 @@ def find_global_max_sample_length():
     return global_max_sample_length
 
 
+def build_arrays(data_folder_path, chunk_file_name):
+    ak_array = read_parquet(os.path.join(data_folder_path, chunk_file_name))
+
+    frac_labels = build_labels_array(
+        ak_array, global_max_sample_length, "Fraction_Label"
+    )
+    tot_labels = build_labels_array(ak_array, global_max_sample_length, "Total_Label")
+    tot_truth_e = build_labels_array(
+        ak_array, global_max_sample_length, "Total_Truth_Energy"
+    )
+
+    feats = build_input_array(ak_array, global_max_sample_length, energy_scale=1000)
+
+    # Save the feats and labels arrays to an NPZ file for each chunk
+    npz_save_path = os.path.join(
+        npz_data_folder_path, f"{chunk_file_name.split('.')[0]}.npz"
+    )
+    np.savez(
+        npz_save_path,
+        feats=feats,
+        frac_labels=frac_labels,
+        tot_labels=tot_labels,
+        tot_truth_e=tot_truth_e,
+    )
+
+
+def build_arrays_wrapper(args):
+    return build_arrays(*args)
+
+
+# Make sure this happens after SAVE_LOC is defined and created if necessary
+for folder in DATA_FOLDERS:
+    folder_path = os.path.join(AWK_SAVE_LOC, folder)
+    os.makedirs(
+        folder_path, exist_ok=True
+    )  # This line ensures the AWK_SAVE_LOC directories exist
+
 global_max_sample_length = find_global_max_sample_length()
 print(f"{global_max_sample_length = }")
 # global_max_sample_length = 278  # placeholder for now
 
-
-for data_folder in DATA_FOLDERS:
+start_time = time.time()
+for data_folder in DATA_FOLDERS[-1:]:
     npz_data_folder_path = os.path.join(NPZ_SAVE_LOC, data_folder)
     os.makedirs(npz_data_folder_path, exist_ok=True)  # Ensure the directory exists
     print(f"Processing data for: {data_folder}")
 
     data_folder_path = os.path.join(AWK_SAVE_LOC, data_folder)
+    chunk_files = [
+        f
+        for f in os.listdir(data_folder_path)
+        if f.startswith("chunk_") and f.endswith(".parquet")
+    ]
     num_chunks = len(chunk_files)
 
-    for i in range(num_chunks):
-        print(f"  Working on chunk {i} of {num_chunks - 1} in {data_folder}...", end="", flush=True)
-        start_time = time.time()
-        # Note: The file name is directly obtained from chunk_files list based on the sorted order
-        chunk_file_name = sorted(chunk_files)[i]
-        ak_array = read_parquet(os.path.join(data_folder_path, chunk_file_name))
-
-        frac_labels = build_labels_array(ak_array, global_max_sample_length, "Fraction_Label")
-        tot_labels = build_labels_array(ak_array, global_max_sample_length, "Total_Label", label_scale = 1)
-        tot_truth_e = build_labels_array(ak_array, global_max_sample_length, "Total_Truth_Energy", label_scale = 1)
-
-        feats = build_input_array(ak_array, global_max_sample_length, energy_scale = 1)
-
-        # Save the feats and labels arrays to an NPZ file for each chunk
-        npz_save_path = os.path.join(npz_data_folder_path, f'chunk_{i}_{data_folder}.npz')
-        np.savez(npz_save_path, feats=feats, frac_labels=frac_labels, tot_labels=tot_labels, tot_truth_e = tot_truth_e)
-        end_time = time.time()
-        print(f"    Saved {data_folder} chunk {i} to {npz_save_path}...", end="")
-        print(f"    Chunk processing took: ", end_time - start_time, "seconds")
-
+    with Pool(processes=NUM_CHUNK_THREADS) as pool:
+        results = list(
+            tqdm(
+                pool.imap_unordered(
+                    build_arrays_wrapper,
+                    zip([data_folder_path] * num_chunks, sorted(chunk_files)),
+                ),
+                total=num_chunks,
+            )
+        )
     print(f"Completed processing data for: {data_folder}")
 
-#ADD STUFF FOR STORING THE METADATA WHEN GENERATING THESE AWKWARD ARRAYS, THEN SAVE IT OR SOMETHINGG FOR MAX SAMPLE LEGNTH? IDK
+end_time = time.time()
+print(f"Processing took: {(end_time - start_time):.2f} seconds")
