c95f3d12a47df0eea3090b4fb342f35ef6a4a752
clissa <clissa.professional@gmail.com>
2024-06-03 10:33:05 +0200

    Update USER logic based on server username.
    This expects "luclissa" and "jhimmens" (to check), with the advantage of not needing to switch manually!

--- python_scripts/data_processing/jets/preprocessing_header.py
+++ python_scripts/data_processing/jets/preprocessing_header.py
@@ -21,17 +21,39 @@ HAS_FIXED_R, FIXED_R, FIXED_Z = (
 
 
 # ===== FIELDS TO CHANGE =====
-add_tracks_as_labels = False
-NUM_EVENTS_PER_CHUNK = 200
-TRAIN_SPLIT_RATIO = 0.55
-VAL_SPLIT_RATIO = 0.3
-# TEST_SPLIT_RATIO is implied to be the remaining percentage
-NUM_THREAD_PER_CHUNK = 25  # root to awk
-NUM_CHUNK_THREADS = 16  # awk to npz
-# OUTPUT_DIRECTORY_NAME = "rho_full/"
-OUTPUT_DIRECTORY_NAME = "ttbar"
-DATASET_NAME = "benchmark"
-# DATASET_NAME = "raw"
+USER = Path.home().name
+if USER == "jhimmens":
+    FRACTIONAL_ENERGY_CUTOFF = 0.5
+    add_tracks_as_labels = False
+    NUM_EVENTS_PER_CHUNK = 200
+    TRAIN_SPLIT_RATIO = 0.55
+    VAL_SPLIT_RATIO = 0.3
+    # TEST_SPLIT_RATIO is implied to be the remaining percentage
+    NUM_THREAD_PER_CHUNK = 25  # root to awk
+    NUM_CHUNK_THREADS = 30  # awk to npz
+    # OUTPUT_DIRECTORY_NAME = "rho_full/"
+    # OUTPUT_DIRECTORY_NAME = "ttbar"
+    OUTPUT_DIRECTORY_NAME = "2000_events_w_fixed_hits/"
+    # DATASET_NAME = "benchmark"
+    DATASET_NAME = "raw"
+    FILE_LOC = "/fast_scratch_1/atlas/pflow/mltree_2000_fixedHits.root"
+    GEO_FILE_LOC = "/fast_scratch_1/atlas/pflow/rho_small.root"
+elif USER == "luclissa":
+    FRACTIONAL_ENERGY_CUTOFF = 0.5
+    add_tracks_as_labels = False
+    NUM_EVENTS_PER_CHUNK = 200
+    TRAIN_SPLIT_RATIO = 0.55
+    VAL_SPLIT_RATIO = 0.3
+    # TEST_SPLIT_RATIO is implied to be the remaining percentage
+    NUM_THREAD_PER_CHUNK = 25  # root to awk
+    NUM_CHUNK_THREADS = 30  # awk to npz
+    # OUTPUT_DIRECTORY_NAME = "rho_full/"
+    # OUTPUT_DIRECTORY_NAME = "ttbar"
+    OUTPUT_DIRECTORY_NAME = "2000_events_w_fixed_hits/"
+    # DATASET_NAME = "benchmark"
+    DATASET_NAME = "test"
+    FILE_LOC = "/eos/home-m/mswiatlo/forLuca/mltree_large.root"
+    GEO_FILE_LOC = "/eos/home-m/mswiatlo/images/truthPerCell/cell_geo.root"
 # ============================
 
 
