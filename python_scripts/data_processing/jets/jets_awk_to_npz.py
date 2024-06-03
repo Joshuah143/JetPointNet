039f359dd21a3fc76f59fb46c2d6cb2998343ce4
clissa <clissa.professional@gmail.com>
2024-06-03 16:20:09 +0200

    Rollback awk->npz to last working script.
    MR requires following TODO:
    - Fix multiple if __main__ blocks
    - Fix indentation
    - check need for update_progress function
    - check speed improvement (?)

--- python_scripts/data_processing/jets/jets_awk_to_npz.py
+++ python_scripts/data_processing/jets/jets_awk_to_npz.py
@@ -1,7 +1,5 @@
 import sys
 from pathlib import Path
-import os
-import threading
 
 REPO_PATH = Path.home() / "workspace/jetpointnet"
 SCRIPT_PATH = REPO_PATH / "python_scripts"
@@ -27,7 +25,7 @@ import pandas as pd
 import os
 import time
 from tqdm.auto import tqdm
-from multiprocessing import Manager, Pool
+from multiprocessing import Pool
 
 
 DATA_FOLDERS = ["train", "val", "test"]
@@ -40,42 +38,31 @@ def read_parquet(filename):
     return ak_array
 
 
-def max_calculation_wrapper(folder_path, filename, max_sample_len_list, n_points_list):
-    full_path = os.path.join(folder_path, filename)
-    ak_array = read_parquet(full_path)
-    max_sample_length, n_points = calculate_max_sample_length(ak_array)
-    print(f"{str(filename).split()[0]} max sample length: {max_sample_length}")
-
-    max_sample_len_list.append(max_sample_length)
-    n_points_list.append(n_points)
-
-
 def find_global_max_sample_length():
-    max_sample_len_manager = Manager()
-    max_sample_len_list = max_sample_len_manager.list()
-    n_points_list = max_sample_len_manager.list()
-
-    with Pool(processes=NUM_CHUNK_THREADS) as pool:
-        for folder in DATA_FOLDERS:
-            folder_path = os.path.join(AWK_SAVE_LOC, folder)
-            for filename in os.listdir(folder_path):
-                if filename.endswith(".parquet"):
-                    pool.apply_async(max_calculation_wrapper, args=(folder_path, filename, max_sample_len_list, n_points_list))
-
-        pool.close()
-        pool.join()
-
-    global_max_sample_length = max(list(max_sample_len_list))
+    hits_count = np.empty(shape=(0, 5))
+    global_max_sample_length = 0
+    for folder in tqdm(DATA_FOLDERS, desc="split loop"):
+        folder_path = os.path.join(AWK_SAVE_LOC, folder)
+        for filename in tqdm(
+            os.listdir(folder_path), desc=f"{folder} set loop", leave=False
+        ):
+            if filename.endswith(".parquet"):
+                full_path = os.path.join(folder_path, filename)
+                ak_array = read_parquet(full_path)
+                max_sample_length, n_points = calculate_max_sample_length(ak_array)
+                hits_count = np.concatenate((hits_count, n_points), axis=0)
+                print("Max sample length found: ", max_sample_length)
+                global_max_sample_length = max(
+                    global_max_sample_length, max_sample_length
+                )
     print(f"Global Max Sample Length: {global_max_sample_length}")
-    
     hits_df = pd.DataFrame(
-        np.vstack(n_points_list), # unsorted
+        hits_count,
         columns=["eventNumber", "trackID", "nHits", "nCell", "nUnfocusHits"],
         dtype=int,
     )
     hits_df.sort_values(["eventNumber", "trackID"], inplace=True)
-
-        # hits_df["nTrack"] = hits_df.groupby(["eventNumber"]).trackID.count().values # L: don't work due to repeated eventNumbers, would need join
+    # hits_df["nTrack"] = hits_df.groupby(["eventNumber"]).trackID.count().values # L: don't work due to repeated eventNumbers, would need join
 
     # dump metadata
     metadata_path = Path(folder_path).parent / "metadata"
@@ -85,7 +72,7 @@ def find_global_max_sample_length():
     return global_max_sample_length
 
 
-def build_arrays(data_folder_path, chunk_file_name, npz_save_path):
+def build_arrays(data_folder_path, chunk_file_name):
     ak_array = read_parquet(os.path.join(data_folder_path, chunk_file_name))
 
     frac_labels = build_labels_array(
@@ -103,7 +90,7 @@ def build_arrays(data_folder_path, chunk_file_name, npz_save_path):
 
     # Save the feats and labels arrays to an NPZ file for each chunk
     npz_save_path = os.path.join(
-        npz_save_path, f"{chunk_file_name.split('.')[0]}.npz"
+        npz_data_folder_path, f"{chunk_file_name.split('.')[0]}.npz"
     )
     np.savez(
         npz_save_path,
@@ -112,10 +99,11 @@ def build_arrays(data_folder_path, chunk_file_name, npz_save_path):
         tot_labels=tot_labels,
         tot_truth_e=tot_truth_e,
     )
-    return # multiproccessing must return to join processes
+
 
 def build_arrays_wrapper(args):
     return build_arrays(*args)
+
 
 if __name__ == "__main__":
 
@@ -126,14 +114,6 @@ if __name__ == "__main__":
             folder_path, exist_ok=True
         )  # This line ensures the AWK_SAVE_LOC directories exist
 
-global_max_sample_length = find_global_max_sample_length()
-# global_max_sample_length = 278  # placeholder for now
-
-all_directory_chunk_folders = []
-all_directory_chunk_files = []
-all_directory_save_folders = []
-all_directory_num_chunks = 0
-if __name__ == "__main__":
     global_max_sample_length = find_global_max_sample_length()
     print(f"{global_max_sample_length = }")
     # global_max_sample_length = 278  # placeholder for now
@@ -145,51 +125,25 @@ if __name__ == "__main__":
         os.makedirs(npz_data_folder_path, exist_ok=True)  # Ensure the directory exists
         print(f"Processing data for: {data_folder}")
 
-    data_folder_path = os.path.join(AWK_SAVE_LOC, data_folder)
-    chunk_files = [
-        f
-        for f in os.listdir(data_folder_path)
-        if f.startswith("chunk_") and f.endswith(".parquet")
-    ]
-    num_chunks = len(chunk_files)
-    all_directory_num_chunks += num_chunks
-    all_directory_chunk_files += sorted(chunk_files)
-    all_directory_chunk_folders += [data_folder_path] * num_chunks
-    all_directory_save_folders += [npz_data_folder_path] * num_chunks
+        data_folder_path = os.path.join(AWK_SAVE_LOC, data_folder)
+        chunk_files = [
+            f
+            for f in os.listdir(data_folder_path)
+            if f.startswith("chunk_") and f.endswith(".parquet")
+        ]
+        num_chunks = len(chunk_files)
 
-
-def update_progress(progress_dict, pbar, all_directory_num_chunks):
-    while True:
-        pbar.n = progress_dict["completed"]
-        pbar.refresh()
-        if progress_dict["completed"] == all_directory_num_chunks:
-            break
-        time.sleep(0.2)
-
-
-if __name__ == "__main__":
-    progress_manager = Manager()
-    progress_dict = progress_manager.dict()
-    progress_dict["completed"] = 0
-
-    pbar = tqdm(total=all_directory_num_chunks)
-
-    def on_task_complete(_):
-        progress_dict["completed"] += 1
-
-    progress_thread = threading.Thread(target=update_progress, args=(progress_dict, pbar, all_directory_num_chunks,))
-    progress_thread.start()
-    data_folder_path = os.path.join(AWK_SAVE_LOC, data_folder)
-
-    with Pool(processes=NUM_CHUNK_THREADS) as pool:
-        for awk_folder, awk_file, npz_folder in zip(all_directory_chunk_folders, all_directory_chunk_files, all_directory_save_folders):
-            pool.apply_async(build_arrays, (awk_folder, awk_file, npz_folder), callback=on_task_complete)
-            
-        pool.close()
-        pool.join()
-
-    pbar.close()
-    progress_thread.join()
+        with Pool(processes=NUM_CHUNK_THREADS) as pool:
+            results = list(
+                tqdm(
+                    pool.imap_unordered(
+                        build_arrays_wrapper,
+                        zip([data_folder_path] * num_chunks, sorted(chunk_files)),
+                    ),
+                    total=num_chunks,
+                )
+            )
+        print(f"Completed processing data for: {data_folder}")
 
     end_time = time.time()
     print(f"Processing took: {(end_time - start_time):.2f} seconds")
