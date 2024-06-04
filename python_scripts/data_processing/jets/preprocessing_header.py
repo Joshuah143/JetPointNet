fab8b6e66d9bd2ab860d15b005a5adb07fa25543
clissa <clissa.professional@gmail.com>
2024-06-04 12:03:04 +0200

    Invert DATASET_NAME and OUTPUT_DIRECTORY_NAME in data paths.
    Now it makes more sense as:
     - DATASET_NAME is the first level and reflects the data content
     - OUTPUT_DIRECTORY_NAME is the second level and reflects where preprocessing is saved. Also, this way it can be used to redirect different preprocessing strategy by changing this name

--- python_scripts/data_processing/jets/preprocessing_header.py
+++ python_scripts/data_processing/jets/preprocessing_header.py
@@ -46,12 +46,11 @@ elif USER == "luclissa":
     VAL_SPLIT_RATIO = 0.3
     # TEST_SPLIT_RATIO is implied to be the remaining percentage
     NUM_THREAD_PER_CHUNK = 25  # root to awk
-    NUM_CHUNK_THREADS = 30  # awk to npz
-    # OUTPUT_DIRECTORY_NAME = "rho_full/"
-    OUTPUT_DIRECTORY_NAME = "ttbar"
-    # OUTPUT_DIRECTORY_NAME = "2000_events_w_fixed_hits/"
-    DATASET_NAME = "benchmark"
-    # DATASET_NAME = "raw"
+    NUM_CHUNK_THREADS = 25  # awk to npz
+    DATASET_NAME = "ttbar"  # or "rho_full/"
+    OUTPUT_DIRECTORY_NAME = (
+        "test"  # "benchmark" # or "raw": NOTE: for tests change this
+    )
     FILE_LOC = "/eos/home-m/mswiatlo/forLuca/mltree_large.root"
     GEO_FILE_LOC = "/eos/home-m/mswiatlo/images/truthPerCell/cell_geo.root"
 # ============================
@@ -78,15 +77,15 @@ MAX_DISTANCE = 0.1
 AWK_SAVE_LOC = (
     REPO_PATH
     / "pnet_data/processed_files"
-    / OUTPUT_DIRECTORY_NAME
+    / DATASET_NAME
     / "AwkwardArrs"
     / f"deltaR={MAX_DISTANCE}"
 )
 NPZ_SAVE_LOC = (
     REPO_PATH
     / "pnet_data/processed_files"
-    / OUTPUT_DIRECTORY_NAME
     / DATASET_NAME
+    / OUTPUT_DIRECTORY_NAME
     / "SavedNpz"
     / f"deltaR={MAX_DISTANCE}"
 )
