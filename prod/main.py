2c586219b914bb7775c992fbe331e5b7e97336d9
Joshua Himmens <jhimmens@triumf.ca>
2024-10-21 06:34:04 +0000

    add prod dir

---
+++ prod/main.py
@@ -0,0 +1,14 @@
+import uproot
+
+def load_from_root(root_file, geo_file, truth) -> uproot.TTree:
+    pass
+
+
+def load_from_args(truth) -> uproot.TTree:
+    pass
+
+
+def main(config):
+    pass
+
+
